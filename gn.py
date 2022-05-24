bab8c2274437931eb215c18fd0fc1cc4c4a82c04
Tom Wang <s22659211@yahoo.com.tw>
2022-05-25 02:41:50 +0800

    加入猜了幾次

--- gn.py
+++ gn.py
@@ -1,14 +1,20 @@
 # 猜數字遊戲
 import random
 r = random.randint(1, 100)
+c = 1
+c = int(c)
 while True:
 	n = input('請輸入1~100的正整數: ')
 	n = int(n)
 	if n == r:
 		print('猜對了')
+		print('你共猜了', c, '次') 
 		break
 	elif n > r:
 		print('比答案大')
 	elif n < r:
 		print('比答案小')
+	c = c + 1
+
+
 
